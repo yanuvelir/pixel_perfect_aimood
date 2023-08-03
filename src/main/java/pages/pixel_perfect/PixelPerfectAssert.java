ae12eaceea2071d7fbbf5a3053956e75048c32e8
yanuvelir <yanuvelir@gmail.com>
2023-08-03 17:14:24 +0300

    allHeaderMainPage all test uncommitted

--- src/main/java/pages/pixel_perfect/PixelPerfectAssert.java
+++ src/main/java/pages/pixel_perfect/PixelPerfectAssert.java
@@ -32,9 +32,9 @@ public class PixelPerfectAssert {
         try {
             File screenshotFile = inputWrapper.getScreenshotAs(OutputType.FILE);
             File destinationFile = new File
-                    (RealLifeScreenShotPath);
-            // To install new screenshots recommit MockFilePath
-//                    (MockFilePath);
+//                    (RealLifeScreenShotPath);
+            // *** To install new screenshots recommit MockFilePath ***
+                    (MockFilePath);
             Files.move(screenshotFile.toPath(), destinationFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
             if (!destinationFile.exists()) {
                 destinationFile.createNewFile();
@@ -122,9 +122,9 @@ public class PixelPerfectAssert {
         try {
             File screenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
             File destinationFile = new File
-                    (RealLifeScreenShotPath);
-            // To install new screenshots recommit MockFilePath
-//                    (MockFilePath);
+//                    (RealLifeScreenShotPath);
+            // *** To install new screenshots recommit MockFilePath ***
+                    (MockFilePath);
             Files.move(screenshotFile.toPath(), destinationFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
             if (!destinationFile.exists()) {
                 destinationFile.createNewFile();
