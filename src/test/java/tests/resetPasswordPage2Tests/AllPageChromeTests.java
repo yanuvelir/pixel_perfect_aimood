e1b172a4b2c4bb6d1e0dda2732594e3e57cf7acd
Olexander Bublik <o.bublik@wpt.dev>
2023-09-08 15:12:57 +0300

    DREAM-989 stage 1 Reset Password page Reset Password page2 firefox and chrome Unversion files

---
+++ src/test/java/tests/resetPasswordPage2Tests/AllPageChromeTests.java
@@ -0,0 +1,202 @@
+package tests.resetPasswordPage2Tests;
+
+import Common.CommonActions;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import tests.base.BaseTest;
+
+import java.io.IOException;
+
+public class AllPageChromeTests extends BaseTest {
+    public AllPageChromeTests() {
+        super("win_chrome");
+    }
+    //HEADER TESTS
+    @Parameters({"resetPassword_page_2", "ScShotDir"})
+    @Test(priority = 1)
+    public void allHeaderResetPasswordPage2(String resetPassword_page_2, String ScShotDir) throws IOException, InterruptedException {
+        // Resolution
+        CommonActions.setBrowserWindowSize("mac 14");
+        //Open a page for test
+        action.openPage(resetPassword_page_2);
+        //Click button "Accept" use of cookies
+        action.ifButtonIsPresentClick("//button[@class='accept-cookies-btn']");
+        Thread.sleep(2000);
+        //Run the Pixel perfect test for a selected element
+        pixelPerfectAssert.byElement("//header[@class='header-wrapper']",
+                ScShotDir+"ResetPassword_page_2/EnChrome/Screenshots_allHeaderResetPasswordPage2/real_life_screenshot.png",
+                ScShotDir+"ResetPassword_page_2/EnChrome/Screenshots_allHeaderResetPasswordPage2/mock_screenshot.png",
+                ScShotDir+"ResetPassword_page_2/EnChrome/Screenshots_allHeaderResetPasswordPage2/difference.png");
+
+    }
+
+    @Parameters({"resetPassword_page_2", "ScShotDir"})
+    @Test(priority = 2)
+    public void getStartedHeaderResetPasswordPage2(String resetPassword_page_2, String ScShotDir) throws IOException, InterruptedException {
+        // Resolution
+        CommonActions.setBrowserWindowSize("mac 14");
+        //Open a page for test
+        action.openPage(resetPassword_page_2);
+        //Hover
+        action.hoverOverAnElementByXpath("//li[@data-testid='head-nav-get-started']");
+        Thread.sleep(100);
+        //Run the Pixel perfect test for a selected element
+        pixelPerfectAssert.byElement("//div[@class='header-drop-down'][1]",
+                ScShotDir+"ResetPassword_page_2/EnChrome/Screenshots_getStartedHeaderResetPasswordPage2/real_life_screenshot.png",
+                ScShotDir+"ResetPassword_page_2/EnChrome/Screenshots_getStartedHeaderResetPasswordPage2/mock_screenshot.png",
+                ScShotDir+"ResetPassword_page_2/EnChrome/Screenshots_getStartedHeaderResetPasswordPage2/difference.png");
+
+    }
+
+    @Parameters({"resetPassword_page_2", "ScShotDir"})
+    @Test(priority = 3)
+    public void learnHeaderResetPasswordPage2(String resetPassword_page_2, String ScShotDir) throws IOException, InterruptedException {
+        // Resolution
+        CommonActions.setBrowserWindowSize("mac 14");
+        //Open a page for test
+        action.openPage(resetPassword_page_2);
+        //Hover
+        action.hoverOverAnElementByXpath("//li[@data-testid='head-nav-learn']");
+        Thread.sleep(100);
+        //Run the Pixel perfect test for a selected element
+        pixelPerfectAssert.byElement("//li[@class='header-nav-item learn-nav-item']/div[@class='header-drop-down']",
+                ScShotDir+"ResetPassword_page_2/EnChrome/Screenshots_learnHeaderResetPasswordPage2/real_life_screenshot.png",
+                ScShotDir+"ResetPassword_page_2/EnChrome/Screenshots_learnHeaderResetPasswordPage2/mock_screenshot.png",
+                ScShotDir+"ResetPassword_page_2/EnChrome/Screenshots_learnHeaderResetPasswordPage2/difference.png");
+
+    }
+
+    @Parameters({"resetPassword_page_2", "ScShotDir"})
+    @Test(priority = 4)
+    public void resourcesHeaderResetPasswordPage2(String resetPassword_page_2, String ScShotDir) throws IOException, InterruptedException {
+        // Resolution
+        CommonActions.setBrowserWindowSize("mac 14");
+        //Open a page for test
+        action.openPage(resetPassword_page_2);
+        //Hover
+        action.hoverOverAnElementByXpath("//li[@data-testid='head-nav-resources']");
+        Thread.sleep(100);
+        //Run the Pixel perfect test for a selected element
+        pixelPerfectAssert.byElement("//nav/ul/li[5]/div[@class='header-drop-down']",
+                ScShotDir+"ResetPassword_page_2/EnChrome/Screenshots_resourcesHeaderResetPasswordPage2/real_life_screenshot.png",
+                ScShotDir+"ResetPassword_page_2/EnChrome/Screenshots_resourcesHeaderResetPasswordPage2/mock_screenshot.png",
+                ScShotDir+"ResetPassword_page_2/EnChrome/Screenshots_resourcesHeaderResetPasswordPage2/difference.png");
+
+    }
+
+//    @Parameters({"resetPassword_page_2", "ScShotDir"})
+//    @Test(priority = 5)
+//    public void langItemHeaderResetPasswordPage2(String resetPassword_page_2, String ScShotDir) throws IOException, InterruptedException {
+//        // Resolution
+//        CommonActions.setBrowserWindowSize("mac 14");
+//        //Open a page for test
+//        action.openPage(resetPassword_page_2);
+//        //Hover
+//        action.hoverOverAnElementByXpath("//div[@data-testid='head-nav-lang']");
+//        //Run the Pixel perfect test for a selected element
+//        pixelPerfectAssert.byElement("//div[@class='header-drop-down'][1]",
+//                ScShotDir+"ResetPassword_page_2/EnChrome/Screenshots_langItemHeaderResetPasswordPage2/real_life_screenshot.png",
+//                ScShotDir+"ResetPassword_page_2/EnChrome/Screenshots_langItemHeaderResetPasswordPage2/mock_screenshot.png",
+//                ScShotDir+"ResetPassword_page_2/EnChrome/Screenshots_langItemHeaderResetPasswordPage2/difference.png");
+//
+//    }
+
+
+    //BODY TESTS
+    @Parameters({"resetPassword_page_2", "ScShotDir"})
+    @Test(priority = 6)
+    public void mainSectionResetPasswordPage2(String resetPassword_page_2, String ScShotDir) throws IOException, InterruptedException {
+        // Resolution
+        CommonActions.setBrowserWindowSize("mac 14");
+        //Open a page for test
+        action.openPage(resetPassword_page_2);
+        //Time
+        Thread.sleep(2000);
+        //Run the Pixel perfect test for a selected element
+        pixelPerfectAssert.byElement("//main[@class='main']",
+                ScShotDir+"ResetPassword_page_2/EnChrome/Screenshots_mainSectionResetPasswordPage2/real_life_screenshot.png",
+                ScShotDir+"ResetPassword_page_2/EnChrome/Screenshots_mainSectionResetPasswordPage2/mock_screenshot.png",
+                ScShotDir+"ResetPassword_page_2/EnChrome/Screenshots_mainSectionResetPasswordPage2/difference.png");
+
+    }
+
+
+
+    //FOOTER TESTS
+    @Parameters({"resetPassword_page_2", "ScShotDir"})
+    @Test(priority = 7)
+    public void allFooterResetPasswordPage2(String resetPassword_page_2, String ScShotDir) throws IOException, InterruptedException {
+        // Resolution
+        CommonActions.setBrowserWindowSize("mac 14");
+        //Open a page for test
+        action.openPage(resetPassword_page_2);
+        //scroll
+        action.moveViewableElementIntoCenter2("//footer[@class='footer-new item']");
+        //Time
+        Thread.sleep(1000);
+        //Run the Pixel perfect test for a selected element
+        pixelPerfectAssert.byElement("//footer[@class='footer-new item']",
+                ScShotDir+"ResetPassword_page_2/EnChrome/Screenshots_allFooterResetPasswordPage2/real_life_screenshot.png",
+                ScShotDir+"ResetPassword_page_2/EnChrome/Screenshots_allFooterResetPasswordPage2/mock_screenshot.png",
+                ScShotDir+"ResetPassword_page_2/EnChrome/Screenshots_allFooterResetPasswordPage2/difference.png");
+
+    }
+
+//    @Parameters({"resetPassword_page_2", "ScShotDir"})
+//    @Test(priority = 8)
+//    public void partnFooterResetPasswordPage2(String resetPassword_page_2, String ScShotDir) throws IOException, InterruptedException {
+//        // Resolution
+//        CommonActions.setBrowserWindowSize("mac 14");
+//        //Open a page for test
+//        action.openPage(resetPassword_page_2);
+//        //scroll
+//        action.moveViewableElementIntoCenter2("//div[@class='partn-container']");
+//        //Time
+//        Thread.sleep(1000);
+//        //Run the Pixel perfect test for a selected element
+//        pixelPerfectAssert.byElement("//div[@class='partn-container']",
+//                ScShotDir+"ResetPassword_page_2/EnChrome/Screenshots_partnFooterResetPasswordPage2/real_life_screenshot.png",
+//                ScShotDir+"ResetPassword_page_2/EnChrome/Screenshots_partnFooterResetPasswordPage2/mock_screenshot.png",
+//                ScShotDir+"ResetPassword_page_2/EnChrome/Screenshots_partnFooterResetPasswordPage2/difference.png");
+//
+//    }
+//
+//    @Parameters({"resetPassword_page_2", "ScShotDir"})
+//    @Test(priority = 9)
+//    public void subFooterResetPasswordPage2(String resetPassword_page_2, String ScShotDir) throws IOException, InterruptedException {
+//        // Resolution
+//        CommonActions.setBrowserWindowSize("mac 14");
+//        //Open a page for test
+//        action.openPage(resetPassword_page_2);
+//        //scroll
+//        action.moveViewableElementIntoCenter2("//div[@class='subfooter']");
+//        //Time
+//        Thread.sleep(1000);
+//        //Run the Pixel perfect test for a selected element
+//        pixelPerfectAssert.byElement("//div[@class='subfooter']",
+//                ScShotDir+"ResetPassword_page_2/EnChrome/Screenshots_subFooterResetPasswordPage2/real_life_screenshot.png",
+//                ScShotDir+"ResetPassword_page_2/EnChrome/Screenshots_subFooterResetPasswordPage2/mock_screenshot.png",
+//                ScShotDir+"ResetPassword_page_2/EnChrome/Screenshots_subFooterResetPasswordPage2/difference.png");
+//
+//    }
+//
+//    @Parameters({"resetPassword_page_2", "ScShotDir"})
+//    @Test(priority = 10)
+//    public void copFooterResetPasswordPage2(String resetPassword_page_2, String ScShotDir) throws IOException, InterruptedException {
+//        // Resolution
+//        CommonActions.setBrowserWindowSize("mac 14");
+//        //Open a page for test
+//        action.openPage(resetPassword_page_2);
+//        //scroll
+//        action.moveViewableElementIntoCenter2("//div[@class='copy-container']");
+//        //Time
+//        Thread.sleep(100);
+//        //Run the Pixel perfect test for a selected element
+//        pixelPerfectAssert.byElement("//div[@class='copy-container']",
+//                ScShotDir+"ResetPassword_page_2/EnChrome/Screenshots_copFooterResetPasswordPage2/real_life_screenshot.png",
+//                ScShotDir+"ResetPassword_page_2/EnChrome/Screenshots_copFooterResetPasswordPage2/mock_screenshot.png",
+//                ScShotDir+"ResetPassword_page_2/EnChrome/Screenshots_copFooterResetPasswordPage2/difference.png");
+//
+//    }
+
+}
